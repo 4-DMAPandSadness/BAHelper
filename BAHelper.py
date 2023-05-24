02e202781afface30cfadc5ed48b6f17fbe74a49
Lucas Tränkle <lucas.traenkle@venus.uni-freiburg.de>
2023-05-24 08:59:47 +0200

    Buttons will now display in the right way. Console output now works. Added reset and clear Button for Buttons and Console.

--- BAHelper.py
+++ BAHelper.py
@@ -21,8 +21,8 @@ class MainWindow(QW.QMainWindow):
 
         """
         self.default_palette = QGuiApplication.palette()
-        # self.ui.Theme.setChecked(False)
-        # self.changeTheme()
+        self.hpToDict("HPger.txt")
+        self.buttons()
         
     def onQuit(self):
         """
@@ -47,38 +47,53 @@ class MainWindow(QW.QMainWindow):
         BAHelper.aboutToQuit.connect(self.onQuit)
         self.ui.actionLight.triggered.connect(lambda: self.changeTheme("light"))
         self.ui.actionDark.triggered.connect(lambda: self.changeTheme("dark"))
-        self.ui.actionGerman.triggered.connect()
-        self.ui.actionEnglish.triggered.connect()
-        self.hpToDict("HPeng.txt")
-        self.buttons()
+        self.ui.actionGerman.triggered.connect(lambda: self.hpToDict("HPger.txt"))
+        self.ui.actionEnglish.triggered.connect(lambda: self.hpToDict("HPeng.txt"))
+        self.ui.pushOutput.clicked.connect(self.temp)
+        self.ui.pushClearConsole.clicked.connect(self.clearConsole)
+        self.ui.pushResetButtons.clicked.connect(self.resetButtons)
         
     def buttons(self):
-        hkey_x = 0
-        pkey_x = 0
+        bh = 0
+        ch = 0
+        dh = 0
+        eh = 0
+        ap = 0
+        bp = 0
+        cp = 0
+        dp = 0
+        ep = 0
         for key in self.HP.keys():
             checkBox = QW.QCheckBox(str(key))
             if "H" in key:
                 if key[1] == "2":
-                    self.ui.hLayout.addWidget(checkBox,hkey_x,0)
+                    self.ui.hLayout.addWidget(checkBox,bh,0)
+                    bh += 1
                 elif key[1] == "3":
-                    self.ui.hLayout.addWidget(checkBox,hkey_x,1)
+                    self.ui.hLayout.addWidget(checkBox,ch,1)
+                    ch += 1
                 elif key[1] == "4":
-                    self.ui.hLayout.addWidget(checkBox,hkey_x,2)
+                    self.ui.hLayout.addWidget(checkBox,dh,2)
+                    dh += 1
                 else:
-                    self.ui.hLayout.addWidget(checkBox,hkey_x,3)
-                hkey_x += 1
+                    self.ui.hLayout.addWidget(checkBox,eh,3)
+                    eh += 1
             elif "P" in key:
                 if key[1] == "1":
-                    self.ui.pLayout.addWidget(checkBox,pkey_x,0)
+                    self.ui.pLayout.addWidget(checkBox,ap,0)
+                    ap += 1
                 elif key[1] == "2":
-                    self.ui.pLayout.addWidget(checkBox,pkey_x,1)
+                    self.ui.pLayout.addWidget(checkBox,bp,1)
+                    bp += 1
                 elif key[1] == "3":
-                    self.ui.pLayout.addWidget(checkBox,pkey_x,2)
+                    self.ui.pLayout.addWidget(checkBox,cp,2)
+                    cp += 1
                 elif key[1] == "4":
-                    self.ui.pLayout.addWidget(checkBox,pkey_x,3)
+                    self.ui.pLayout.addWidget(checkBox,dp,3)
+                    dp += 1
                 else:
-                    self.ui.pLayout.addWidget(checkBox,pkey_x,4)
-                pkey_x += 1
+                    self.ui.pLayout.addWidget(checkBox,ep,4)
+                    ep += 1
         
     def changeTheme(self,theme):
         """
@@ -111,12 +126,31 @@ class MainWindow(QW.QMainWindow):
     def hpToDict(self,filename):
         self.HP = pd.read_csv(filename, delimiter="\t", index_col=0, header=None).to_dict()[1]
 
-    def output(self):
-        if self.ui.language.text() == "german":
-            self.hpToDict("HPger.txt")
-        else:
-            self.hpToDict("HPeng.txt")
-        self.buttons()
+    def clearConsole(self):
+        self.ui.textOutput.clear()
+        
+    def resetButtons(self):
+        for checkBox in self.findChildren(QW.QCheckBox):
+            checkBox.setChecked(False)
+
+    def temp(self):
+        self.Hshort = []
+        for HcheckBox in self.findChildren(QW.QCheckBox):
+            if HcheckBox.isChecked() == True and HcheckBox.text()[0] == "H":
+                self.Hshort.append(HcheckBox.text())
+        self.Pshort = []
+        for PcheckBox in self.findChildren(QW.QCheckBox):
+            if PcheckBox.isChecked() == True and PcheckBox.text()[0] == "P":
+                self.Pshort.append(PcheckBox.text())
+        for short in self.Hshort:
+            self.ui.textOutput.append(short + ": " + self.HP[short])
+        for short in self.Pshort:
+            self.ui.textOutput.append(short + ": " + self.HP[short])
+        hNum = str(self.Hshort).replace("[", "").replace("]", "").replace("'","")
+        self.ui.textOutput.append(hNum)
+        pNum = str(self.Pshort).replace("[", "").replace("]", "").replace("'","")
+        self.ui.textOutput.append(pNum)
+    
     #for every selected checkbox in h snetences
     #  add to list
     # dito for p sentences
@@ -125,7 +159,6 @@ class MainWindow(QW.QMainWindow):
     # open popup mit ergebnissen
     # copy to clipboard button?
 
-            
 #####################################APP#######################################            
             
 if __name__ == '__main__':
