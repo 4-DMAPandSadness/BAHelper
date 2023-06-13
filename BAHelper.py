6f4edec24326f9c82990bfdeabae7b00497e5783
Lucas Tränkle <lucas.traenkle@venus.uni-freiburg.de>
2023-06-13 09:39:14 +0200

    Added tooltips. Added fulltext output.

--- BAHelper.py
+++ BAHelper.py
@@ -3,6 +3,7 @@ from PyQt5.uic import loadUi
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPalette, QColor, QGuiApplication
 import pandas as pd
+import numpy as np
 
 class MainWindow(QW.QMainWindow):
     def __init__(self): 
@@ -10,6 +11,8 @@ class MainWindow(QW.QMainWindow):
         self.ui = loadUi("BAHelper_ui.ui",self)
         self.startUp()
         self.functionality()
+        self.AllH = []
+        self.AllP = []
   
     def startUp(self):
         """
@@ -22,6 +25,7 @@ class MainWindow(QW.QMainWindow):
         """
         self.default_palette = QGuiApplication.palette()
         self.hpToDict("HPger.txt")
+        self.ui.labelOutput.setText("Output Language: German")
         self.buttons()
         
     def onQuit(self):
@@ -47,11 +51,21 @@ class MainWindow(QW.QMainWindow):
         BAHelper.aboutToQuit.connect(self.onQuit)
         self.ui.actionLight.triggered.connect(lambda: self.changeTheme("light"))
         self.ui.actionDark.triggered.connect(lambda: self.changeTheme("dark"))
-        self.ui.actionGerman.triggered.connect(lambda: self.hpToDict("HPger.txt"))
-        self.ui.actionEnglish.triggered.connect(lambda: self.hpToDict("HPeng.txt"))
-        self.ui.pushOutput.clicked.connect(self.temp)
+        self.ui.actionGerman.triggered.connect(self.german)
+        self.ui.actionEnglish.triggered.connect(self.english)
+        self.ui.pushOutput.clicked.connect(self.singleOutput)
         self.ui.pushClearConsole.clicked.connect(self.clearConsole)
         self.ui.pushResetButtons.clicked.connect(self.resetButtons)
+        self.ui.pushCopy.clicked.connect(self.copyToClipboard)
+        self.ui.pushOutputAll.clicked.connect(self.fullOutput)
+        
+    def german(self):
+        self.hpToDict("HPger.txt")
+        self.ui.labelOutput.setText("Output Language: German")
+        
+    def english(self):
+        self.hpToDict("HPeng.txt")
+        self.ui.labelOutput.setText("Output Language: English")
         
     def buttons(self):
         bh = 0
@@ -132,33 +146,47 @@ class MainWindow(QW.QMainWindow):
     def resetButtons(self):
         for checkBox in self.findChildren(QW.QCheckBox):
             checkBox.setChecked(False)
+            
+    def copyToClipboard(self):
+        self.ui.textOutput.selectAll()
+        self.ui.textOutput.copy()
 
-    def temp(self):
-        self.Hshort = []
+    def singleOutput(self):
+        Hshort = []
         for HcheckBox in self.findChildren(QW.QCheckBox):
             if HcheckBox.isChecked() == True and HcheckBox.text()[0] == "H":
-                self.Hshort.append(HcheckBox.text())
-        self.Pshort = []
+                Hshort.append(HcheckBox.text())
+        Pshort = []
         for PcheckBox in self.findChildren(QW.QCheckBox):
             if PcheckBox.isChecked() == True and PcheckBox.text()[0] == "P":
-                self.Pshort.append(PcheckBox.text())
-        for short in self.Hshort:
-            self.ui.textOutput.append(short + ": " + self.HP[short])
-        for short in self.Pshort:
-            self.ui.textOutput.append(short + ": " + self.HP[short])
-        hNum = str(self.Hshort).replace("[", "").replace("]", "").replace("'","")
+                Pshort.append(PcheckBox.text())
+        for H in Hshort:
+            if H not in self.AllH:
+                self.AllH.append(H)
+        for P in Pshort:
+            if P not in self.AllP:
+                self.AllP.append(P)
+        hNum = str(Hshort).replace("[", "").replace("]", "").replace("'","")
         self.ui.textOutput.append(hNum)
-        pNum = str(self.Pshort).replace("[", "").replace("]", "").replace("'","")
+        self.ui.textOutput.append("\n")
+        pNum = str(Pshort).replace("[", "").replace("]", "").replace("'","")
         self.ui.textOutput.append(pNum)
-    
-    #for every selected checkbox in h snetences
-    #  add to list
-    # dito for p sentences
-    # for every key in list
-    # give key \t value
-    # open popup mit ergebnissen
-    # copy to clipboard button?
+        self.ui.textOutput.append("\n")
+        self.resetButtons()
 
+    def fullOutput(self):
+        for short in self.AllH:
+            fulltext = short + ": " + self.HP[short]
+            self.ui.textOutput.append(fulltext)
+        self.ui.textOutput.append("\n")
+        for short in self.AllP:
+            fulltext = short + ": " + self.HP[short]
+            self.ui.textOutput.append(fulltext)
+        
+###############################################################################
+
+        
+        
 #####################################APP#######################################            
             
 if __name__ == '__main__':
